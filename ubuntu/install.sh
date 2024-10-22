404de8e20c87224e20d45007bc9c72d23aa09929
halink0803 <halink0803@gmail.com>
2024-10-22 17:39:49 +0700

    update install script

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -1,2 +1,14 @@
 #!bin/bash
 
+# update apt
+sudo apt update
+
+# install essential
+sudo apt install build-essential
+sudo apt install curl
+
+# install zsh
+sudo apt install zsh
+
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
